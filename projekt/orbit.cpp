d9183a9c44b919553440933e9ecf8375f29b00f6
pkin9 <dominika_urbanczyk@o2.pl>
2022-08-07 13:48:12 +0100

    .

--- projekt/orbit.cpp
+++ projekt/orbit.cpp
@@ -10,7 +10,7 @@ void Velocity(double Standard_Gravitational_Parameter, double Distance, double S
 
 double Distance(std::string name, double Semi_Major_Axis_AU, double Semi_Minor_Axis_AU) {
         double Distance, Distance_AU;
-        if (name == "Perapsis") {
+        if (name == "Periapsis") {
             Distance_AU = Semi_Major_Axis_AU - std::pow(Semi_Major_Axis_AU * Semi_Major_Axis_AU - Semi_Minor_Axis_AU * Semi_Minor_Axis_AU, 0.5);
             Distance = Distance_AU * 149597871;
         }
@@ -29,7 +29,7 @@ Orbit::Orbit(double Semi_Major_Axis, double Eccentricity) {
 
     double Semi_Minor_Axis_AU, Semi_Minor_Axis; //a.u., km
     double Distance_In_Apoapsis_AU, Distance_In_Apoapsis; //a.u., km
-    double Distance_In_Perapsis_AU, Distance_In_Perapsis; //a.u., km
+    double Distance_In_Periapsis_AU, Distance_In_Periapsis; //a.u., km
     double Mass_of_Sun = 1.989E30; //kg
     double Gravitational_Constant = 6.674E-11; //m^3 /kg * s^2
     double Standard_Gravitational_Parameter = Gravitational_Constant * Mass_of_Sun * 1E-9;;  // km ^ 3 / s^2
@@ -43,8 +43,8 @@ Orbit::Orbit(double Semi_Major_Axis, double Eccentricity) {
         << "Eccentricity: " << Eccentricity << std::endl
         << "Standard gravitational parameter: " << Standard_Gravitational_Parameter << " km^3/s^2\n" << std::endl;
     
-    Distance_In_Perapsis = Distance("Perapsis", Semi_Major_Axis_AU, Semi_Minor_Axis_AU);
-    Velocity(Standard_Gravitational_Parameter, Distance_In_Perapsis, Semi_Minor_Axis);
+    Distance_In_Periapsis = Distance("Periapsis", Semi_Major_Axis_AU, Semi_Minor_Axis_AU);
+    Velocity(Standard_Gravitational_Parameter, Distance_In_Periapsis, Semi_Minor_Axis);
        
     Distance_In_Apoapsis = Distance("Apoapsis", Semi_Major_Axis_AU, Semi_Minor_Axis_AU);
     Velocity(Standard_Gravitational_Parameter, Distance_In_Apoapsis, Semi_Major_Axis);
